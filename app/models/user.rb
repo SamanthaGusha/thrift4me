55314876a6b316890d6324e9760605a6e8b09738
Finn Mclaughlin <finnmacm@gmail.com>
2024-03-05 14:15:53 +0200

    pushing authentication function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bids
+  has_many :clothes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
