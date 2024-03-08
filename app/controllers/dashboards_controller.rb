3b1eb130994ac9943c64ee18ac724b36ce04eb97
Samantha Gusha <samanthagusha@icloud.com>
2024-03-08 02:11:30 +0200

    not fully done again

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,18 +1,43 @@
 class DashboardsController < ApplicationController
 
   def index
-    @pending_rentals = current_user.rentals.select{ |rental| rental.status["pending"] }
+    # @pending_rentals = current_user.rentals.select{ |rental| rental.status["pending"] }
+    @selected_dashboard = params[:dasboard] || 'my_rentals'
+
+    @rental_requests = current_user.rental_requests.includes(:clothing)
+
+    case @selected_dashboard
+    when 'my_rentals'
+      @rental_requests = current_user.rental_requests.includes(:clothing)
+      # @rental_requests.where(renter: current_user)
+    when 'rented_items'
+      @rental_requests = @rental_requests.where(rentee: current_user)
+    else
+      raise "Invalid dashboard selected:"
+    end
+
+    @past_rentals = current_user.past_rentals
   end
 
-  def accept
-    @rental = Rental.find(params[:id])
-    @rental.update(status: 'accepted')
-    redirect_to user_dashboard_path, notice: 'Rental status updated!'
+  def update_requests
+    @rental_request = Rental.find(params[:id])
+    authorize! :update, @rental_request
+
+    case params[:action]
+    when 'accept'
+      @rental_request.update(status: :accepted)
+    when 'reject'
+      @rental_request.update(status: :rejected)
+    else
+      'pending'
+    end
+    redirect_to rentee_user_dashboard_path(params[:id]), notice: "Request #{@rental_request.id} has been #{@rental_request.status}"
   end
 
-  def renter
-    @active_rentals = current_user.rentals.select{ |rental| rental.status["accepted"] }
-  end
+  private
 
+  def past_rentals
+    current_user.rental_requests.where('rental_end_date < ?', Date.today).includes(:clothing)
+  end
 
 end
